dde17805780bbc79a20ca929fce287b8dbb9c0d6
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-16 13:30:28 +0530

    expense filter and expense chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React from "react";
-import "./App.css";
+import { useState } from "react";
+import Expenses from "./Expenses/Expenses";
+import NewExpense from "./NewExpense/NewExpense";
+import { FormDataType } from "./Interfaces&Types/FormData";
+
+// const expenses = [
+//   {
+//     id: "e1",
+//     title: "Toilet Paper",
+//     amount: 94.12,
+//     date: new Date(2020, 7, 14),
+//   },
+//   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+//   {
+//     id: "e3",
+//     title: "Car Insurance",
+//     amount: 294.67,
+//     date: new Date(2021, 2, 28),
+//   },
+//   {
+//     id: "e4",
+//     title: "New Desk (Wooden)",
+//     amount: 450,
+//     date: new Date(2021, 5, 12),
+//   },
+// ];
+
+const expenses: FormDataType[] = [];
 
 function App() {
+  const [expense, setExpense] = useState(expenses);
+
+  const expenseDataHandler = function (formData: FormDataType) {
+    setExpense((prev) => [formData, ...prev]);
+  };
+
+  const yearArray = expense.map((exp) => new Date(exp.date).getFullYear());
+
   return (
-    <div className="App">
+    <>
       <h1>Expense Tracker</h1>
-    </div>
+      <NewExpense onGettingData={expenseDataHandler} />
+      <Expenses items={expense} year={new Set(yearArray)} />
+    </>
   );
 }
 
---
+++ src/Expenses/Expenses.tsx
@@ -0,0 +1,38 @@
+import "./Expenses.css";
+import Card from "../Card-component/Card";
+import ExpenseFilter from "../expenseFilter/ExpenseFilter";
+import { SetStateAction, useState } from "react";
+import ExpensesList from "./ExpenseList";
+import ExpensesChart from "./ExpensesChart";
+import { FormDataType } from "../Interfaces&Types/FormData";
+
+type ExpensesProps = {
+  items: FormDataType[];
+  year: Set<number>;
+};
+
+function Expenses(props: ExpensesProps) {
+  const [selectedYear, setSelectedYear] = useState("select year");
+
+  const filterChangehandler = function (selectedValue: SetStateAction<string>) {
+    setSelectedYear(selectedValue);
+  };
+
+  let filteredExpense = props.items;
+
+  filteredExpense =
+    selectedYear !== "select year"
+      ? props.items.filter(
+          (item) => +selectedYear === new Date(item.date).getFullYear()
+        )
+      : filteredExpense;
+
+  return (
+    <Card className="expenses">
+      <ExpenseFilter onChangeFilter={filterChangehandler} year={props.year} />
+      <ExpensesChart dataPoints={filteredExpense} />
+      <ExpensesList items={filteredExpense} />
+    </Card>
+  );
+}
+export default Expenses;
