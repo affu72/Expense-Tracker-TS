42d5ff14b2b484c3ca4e679299205d36179c786b
Afzal Nomani <afzalnomani1@gmail.com>
2023-03-20 11:33:33 +0530

    initial data updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,12 @@
 import { useState } from "react";
 import Expenses from "./Expenses/Expenses";
 import NewExpense from "./NewExpense/NewExpense";
-import { FormDataType, ExpenseCategory } from "./Interfaces&Types/FormData";
+import { FormDataType } from "./Interfaces&Types/FormData";
 import "./App.css";
-const expenses: FormDataType<string>[] = [
-  {
-    title: "Shopping",
-    amount: 153,
-    date: new Date("2021-02-10T06:16:35.876Z"),
-    person: "Mary",
-    id: "4k4ty4zry6jm4w6",
-    category: ExpenseCategory.Groceries,
-  },
-  {
-    title: "Groceries",
-    amount: 971,
-    date: new Date("2022-09-13T13:23:59.266Z"),
-    person: "Tom",
-    id: "um8p3yq3vqkuxn3",
-    category: ExpenseCategory.Groceries,
-  },
-  {
-    title: "Dinner",
-    amount: 719,
-    date: new Date("2022-03-28T00:03:10.331Z"),
-    person: "John",
-    id: "x19x66e3nb8q3cu",
-    category: ExpenseCategory.Rent,
-  },
-  {
-    title: "Movie",
-    amount: 783,
-    date: new Date("2021-10-06T11:24:35.644Z"),
-    person: "Bob",
-    id: "m95m9xg6n73m4k4",
-    category: ExpenseCategory.Groceries,
-  },
-  {
-    title: "Gas",
-    amount: 426,
-    date: new Date("2021-05-25T14:25:22.005Z"),
-    person: "Alice",
-    id: "d8q3h3n9x9x6x1z",
-    category: ExpenseCategory.Transportation,
-  },
-];
+import expensesData from "./ExpenseData";
+
 function App() {
-  const [expense, setExpense] = useState(expenses);
+  const [expense, setExpense] = useState(expensesData);
 
   const expenseDataHandler = function (formData: FormDataType<string>) {
     setExpense((prev) => [formData, ...prev]);
